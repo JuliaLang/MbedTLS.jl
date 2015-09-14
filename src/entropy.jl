66d960720243718f9ed7d4711f0097d668aaf03f
Jon Malmaud <malmaud@gmail.com>
2015-09-14 15:56:16 -0400

    Increase bytes allocated for entropy_context

--- src/entropy.jl
+++ src/entropy.jl
@@ -3,7 +3,7 @@ type Entropy
 
     function Entropy()
         ctx = new()
-        ctx.data = Libc.malloc(2000)  # 1024
+        ctx.data = Libc.malloc(100000)  # Exact byte count is 75088; playing it safe with some buffer
         ccall((:mbedtls_entropy_init, MBED_CRYPTO), Void, (Ptr{Void},), ctx.data)
         finalizer(ctx, ctx->begin
             ccall((:mbedtls_entropy_free, MBED_CRYPTO), Void, (Ptr{Void},), ctx.data)
