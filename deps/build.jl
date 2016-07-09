3ecd431753639813087f2a06973442c69b18d42e
Martijn Visser <mgvisser@gmail.com>
2016-07-09 17:48:32 +0200

    check mbedtls_version_get_string in libmbedcrypto
    `mbedtls_version_get_string` only appears in `libmbedcrypto`
    Fixes #49

--- deps/build.jl
+++ deps/build.jl
@@ -17,8 +17,8 @@ function validate_mbed(name, handle)
     end
 end
 
-mbed = library_dependency("libmbedtls", aliases=["libmbedtls", "libmbedtls.2.1.1"], validate=validate_mbed)
-mbed_crypto = library_dependency("libmbedcrypto", aliases=["libmbedcrypto", "libmbedcrypto.2.1.1"])
+mbed = library_dependency("libmbedtls", aliases=["libmbedtls", "libmbedtls.2.1.1"])
+mbed_crypto = library_dependency("libmbedcrypto", aliases=["libmbedcrypto", "libmbedcrypto.2.1.1"], validate=validate_mbed)
 mbed_x509 = library_dependency("libmbedx509", aliases=["libmbedx509", "libmbedx509.2.1.1"])
 
 mbed_all = [mbed, mbed_crypto, mbed_x509]
