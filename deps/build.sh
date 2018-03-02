226d398a17135771a3a2983b5d1929d3b301d344
quinnj <quinn.jacobd@gmail.com>
2018-03-02 11:45:38 -0700

    Fixups for build.jl and build.sh

--- deps/build.sh
+++ deps/build.sh
@@ -5,12 +5,19 @@ if ! cmake --version > /dev/null 2>&1; then
   exit 1
 fi
 
-git clone https://github.com/ARMmbed/mbedtls.git
-VERSION="2.7.1"
-git checkout mbedtls-$VERSION
-cd mbedtls/
+VERSION="2.7.0"
+
+if [ -z "$" ]; then
+    wget https://tls.mbed.org/download/mbedtls-$VERSION-apache.tgz
+    tar xfz mbedtls-$VERSION-apache.tgz
+else
+    wget https://tls.mbed.org/download/mbedtls-$VERSION-gpl.tgz
+    tar xfz mbedtls-$VERSION-gpl.tgz
+fi
+
+cd mbedtls-$VERSION
 cmake -DUSE_SHARED_MBEDTLS_LIBRARY=On .
 make lib
 cd ..
 mkdir -p usr/lib
-cp mbedtls/library/libmbed*.* usr/lib/
+cp mbedtls-$VERSION/library/libmbed*.* usr/lib/
