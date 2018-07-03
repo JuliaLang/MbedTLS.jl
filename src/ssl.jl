84c742ffcf7ecfa43c332b63f6ee2b526a40b93b
quinnj <quinn.jacobd@gmail.com>
2018-07-02 21:04:30 -0600

    Add getsockname for SSLContext

--- src/ssl.jl
+++ src/ssl.jl
@@ -382,6 +382,8 @@ function hostname!(ctx::SSLContext, hostname)
       (Ptr{Cvoid}, Cstring), ctx.data, hostname)
 end
 
+Compat.Sockets.getsockname(ctx::SSLContext) = Compat.Sockets.getsockname(ctx.bio)
+
 const c_send = Ref{Ptr{Cvoid}}(C_NULL)
 const c_recv = Ref{Ptr{Cvoid}}(C_NULL)
 const c_dbg = Ref{Ptr{Cvoid}}(C_NULL)
