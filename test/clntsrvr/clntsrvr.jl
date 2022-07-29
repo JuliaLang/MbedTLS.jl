21808880ae0cdf5f620a4f5049b6139929dc344f
Jameson Nash <vtjnash@gmail.com>
2022-07-29 12:14:35 -0400

    handle normal stream EOF (TLS v1.3 feature)
    Implemented per mbedtls_ssl_read documentation:
    https://github.com/Mbed-TLS/mbedtls/blob/53546ea099f6f53d0be653a64accd250e170337f/include/mbedtls/ssl.h#L2999

--- test/clntsrvr/clntsrvr.jl
+++ test/clntsrvr/clntsrvr.jl
@@ -26,27 +26,47 @@ function testclntsrvr(certfile, keyfile)
     outbuff = ones(UInt8, 100) * UInt8(65)
     trigger = Channel{Bool}(1)
     port = UInt16(0)
-    
-    @async begin
-        (port, server) = listenany(8000)
-        @info("listening on port $port")
-        put!(trigger, true)
+    local clntconn, srvrconn
+
+    # setup a watchdog kill-switch
+    t = Timer(10) do t
+        @isdefined(clntconn) && close(clntconn)
+        @isdefined(srvrconn) && close(srvrconn)
+        close(trigger)
+        @test "test failed to complete within timeout"
+    end
+
+    (port, server) = listenany(8000)
+    @info("listening on port $port")
+
+    r = @async begin
         srvrconn = sslaccept(server, certfile, keyfile)
+        close(server)
         inbuff = read(srvrconn, 100)
         @test inbuff == outbuff
         put!(trigger, true)
+        inbuff2 = read(srvrconn, 1000)
+        @test inbuff2 == outbuff
+        put!(trigger, true)
+        close(srvrconn)
     end
+    bind(trigger, r)
 
-    take!(trigger)
     @info("connecting to port $port")
     clntconn = sslconnect("127.0.0.1", port)
     @test write(clntconn, outbuff) == 100
-    @async begin
-        sleep(10)
-        put!(trigger, false)
-    end
-
     @test take!(trigger)
+    outbuff .*= 2
+    @test write(clntconn, outbuff) == 100
+    close(clntconn)
+    @test take!(trigger)
+    wait(r)
+
+    close(t)
 end
 
-testclntsrvr(joinpath(dirname(@__FILE__), "test.cert"), joinpath(dirname(@__FILE__), "test.key"))
+@testset "testclntsrvr" begin
+    testclntsrvr(
+        joinpath(@__DIR__, "test.cert"),
+        joinpath(@__DIR__, "test.key"))
+end
