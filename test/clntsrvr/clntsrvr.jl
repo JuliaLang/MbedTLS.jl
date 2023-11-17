9a5c1f87b72298ed41d5de78cb8485cda3dd9b6d
Sheldon Hearn <sheldon.hearn@netstock.co>
2023-11-17 08:57:36 +0200

    Demonstrate strerror regression
    Re JuliaLang/MbedTLS.jl#274

--- test/clntsrvr/clntsrvr.jl
+++ test/clntsrvr/clntsrvr.jl
@@ -12,9 +12,8 @@ function sslaccept(server, certfile, keyfile)
     return sslconn
 end
 
-function sslconnect(dest, port)
+function sslconnect(dest, port, sslconfig = MbedTLS.SSLConfig(false))
     conn = connect(dest, port)
-    sslconfig = MbedTLS.SSLConfig(false)
     sslconn = MbedTLS.SSLContext()
     MbedTLS.setup!(sslconn, sslconfig)
     MbedTLS.set_bio!(sslconn, conn)
@@ -65,8 +64,53 @@ function testclntsrvr(certfile, keyfile)
     close(t)
 end
 
+function testverify(certfile, keyfile, badcafile)
+    outbuff = ones(UInt8, 100) * UInt8(65)
+    trigger = Channel{Bool}(1)
+    port = UInt16(0)
+    local clntconn, srvrconn
+
+    # setup a watchdog kill-switch
+    t = Timer(10) do t
+        @isdefined(clntconn) && close(clntconn)
+        @isdefined(srvrconn) && close(srvrconn)
+        close(trigger)
+        @test "test failed to complete within timeout"
+    end
+
+    (port, server) = listenany(8000)
+    @info("listening on port $port")
+
+    r = @async begin
+        try
+            srvrconn = sslaccept(server, certfile, keyfile)
+            close(server)
+        catch e
+        end
+        put!(trigger, true)
+        @isdefined(srvrconn) && close(srvrconn)
+    end
+    bind(trigger, r)
+
+    @info("connecting to port $port")
+    badsslconfig = MbedTLS.SSLConfig(true)
+    badsslconfig.chain = MbedTLS.crt_parse(read(badcafile, String))
+    @test_throws "Certificate verification failed" sslconnect("127.0.0.1", port, badsslconfig)
+    @test take!(trigger)
+    wait(r)
+
+    close(t)
+end
+
 @testset "testclntsrvr" begin
     testclntsrvr(
         joinpath(@__DIR__, "test.cert"),
         joinpath(@__DIR__, "test.key"))
+end
+
+@testset "testverify" begin
+    testverify(
+        joinpath(@__DIR__, "test.cert"),
+        joinpath(@__DIR__, "test.key"),
+        joinpath(@__DIR__, "badca.cert"))
 end
